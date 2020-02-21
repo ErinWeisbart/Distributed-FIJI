125f72d80928a4865da018660c287b9bdcab9f02
Beth Cimini <bethac07@users.noreply.github.com>
2020-02-21 12:16:16 -0500

    add MIN_FILE_SIZE, remove options we won't use

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Constants (User configurable)
 
-APP_NAME = 'DistributedCP'                # Used to generate derivative names unique to the application.
+APP_NAME = 'DistributedFIJI'                # Used to generate derivative names unique to the application.
 
 # DOCKER REGISTRY INFORMATION:
-DOCKERHUB_TAG = 'cellprofiler/distributed-cellprofiler:1.2.1_3.1.9'
+DOCKERHUB_TAG = 'bethcimini/distributed-fiji:latest'
 
 # AWS GENERAL SETTINGS:
 AWS_REGION = 'us-east-1'
@@ -15,15 +15,10 @@ AWS_BUCKET = 'your-bucket-name'
 ECS_CLUSTER = 'default'
 CLUSTER_MACHINES = 3
 TASKS_PER_MACHINE = 1
-EBS_VOL_SIZE = 30                       # In GB.  Minimum allowed is 22.
+EBS_VOL_SIZE = 30                       # In GB.  Minimum allowed is 22.  Docker will get this - 2 GB
 
 # DOCKER INSTANCE RUNNING ENVIRONMENT:
-DOCKER_CORES = 1                        # Number of CellProfiler processes to run inside a docker container
-CPU_SHARES = DOCKER_CORES * 1024        # ECS computing units assigned to each docker container (1024 units = 1 core)
 MEMORY = 4096                           # Memory assigned to the docker container in MB
-SECONDS_TO_START = 0*60                 # Wait before the next CP process is initiated to avoid memory collisions
-DOCKER_BASE_SIZE = 10                   # In GB; default is 10.  The amount of hard disk space each docker container uses.
-                                        # EBS_VOL_SIZE should be >= DOCKER_BASE_SIZE * TASKS_PER_MACHINE
 
 # SQS QUEUE INFORMATION:
 SQS_QUEUE_NAME = APP_NAME + 'Queue'
@@ -36,3 +31,4 @@ LOG_GROUP_NAME = APP_NAME
 # REDUNDANCY CHECKS
 CHECK_IF_DONE_BOOL = 'True'  #True or False- should it check if there are a certain number of non-empty files and delete the job if yes?
 EXPECTED_NUMBER_FILES = 7    #What is the number of files that trigger skipping a job?
+MIN_FILE_SIZE_BYTES = 1      #What is the minimal number of bytes an object should be to "count"?
